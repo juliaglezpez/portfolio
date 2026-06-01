dea4f0b51beb6288221979a2a79661da6d24ee44
juliaglezpez <juliaglezpez@gmail.com>
2026-06-02 00:09:36 +0200

    Implementación de galerías dinámicas para las páginas de categoría
    Desarrollo e integración de galerías dinámicas dentro de la página de gráfico, permitiendo mostrar de forma automática los proyectos correspondientes a cada mención o área de especialización. Se ha configurado la estructura necesaria para gestionar y presentar el contenido de manera organizada, facilitando la escalabilidad del portfolio y mejorando la experiencia de navegación entre categorías y proyectos.

---
+++ js/proyectos.js
@@ -0,0 +1,77 @@
+/*FILTRADO DINÁMICO*/
+const proyectos = [
+  {
+    titulo: "La Casa Encendida",
+    categoria: "branding",
+    imagen: "img/cards/la_casa_encendida.png",
+    pagina: "casa-encendida.html",
+  },
+
+  {
+    titulo: "Camden Rats Sessions",
+    categoria: "carteleria",
+    imagen: "img/cards/candem_rats.png",
+    pagina: "camden-rats.html",
+  },
+];
+
+function renderizarProyectos(lista) {
+  const contenedor = document.querySelector("#galeria");
+
+  contenedor.innerHTML = "";
+
+  lista.forEach(function (proyecto) {
+    contenedor.insertAdjacentHTML(
+      "beforeend",
+
+      `
+<div class="col-12">
+
+  <article class="proyecto__tarjeta">
+
+    <div class="row g-0 align-items-stretch">
+
+      <div class="col-12 col-lg-8">
+
+        <div class="proyecto__contenido">
+
+          <p class="proyecto__categoria">
+            ${proyecto.categoria}
+          </p>
+
+          <h3 class="proyecto__titulo">
+            ${proyecto.titulo}
+          </h3>
+
+          <a
+            href="${proyecto.pagina}"
+            class="proyecto__enlace">
+
+            Ver proyecto →
+
+          </a>
+
+        </div>
+
+      </div>
+
+      <div class="col-12 col-lg-4">
+
+        <img
+          src="${proyecto.imagen}"
+          alt="${proyecto.titulo}"
+          class="proyecto__imagen"
+        >
+
+      </div>
+
+    </div>
+
+  </article>
+
+</div>
+`,
+    );
+  });
+}
+renderizarProyectos(proyectos);
